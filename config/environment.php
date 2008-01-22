b56c479d68b37d2aef939ee1476dccff6396ed2e
Markus Koller <markus-koller@gmx.ch>
2008-01-22 20:30:22 +0000

    move scripts to lib/script

--- config/environment.php
+++ config/environment.php
@@ -11,8 +11,8 @@
    define('LIB',          ROOT.'lib/');
    define('CONFIG',       ROOT.'config/');
    define('LOG',          ROOT.'log/');
-   define('SCRIPT',       ROOT.'script/');
    define('TEST',         ROOT.'test/');
+   define('SCRIPT',       LIB.'script/');
    define('FIXTURES',     TEST.'fixtures/');
 
    # Application paths
