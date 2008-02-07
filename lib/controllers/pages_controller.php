41b2a032db6417e9bf4eebdf5d59b4a3ba156852
Markus Koller <markus-koller@gmx.ch>
2008-02-07 21:02:17 +0000

    PagesController: set title, catch path traversal attacks

--- lib/controllers/pages_controller.php
+++ lib/controllers/pages_controller.php
@@ -10,8 +10,12 @@
    class PagesController extends ApplicationController
    {
       function show($path) {
+         # Catch path traversal attacks
+         $path = ltrim($path, './');
+
          if ($template = self::find_template("pages/$path") or
              $template = self::find_template("pages/$path/index")) {
+            $this->set('title', str_replace('/', ' - ', $path));
             $this->render($template);
          } else {
             raise(MissingTemplate);
