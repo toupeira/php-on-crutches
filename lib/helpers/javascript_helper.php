1684135baec3e1574e7525adf8bfa6339aeab82c
Markus Koller <markus-koller@gmx.ch>
2008-04-27 22:04:08 +0000

    toggle_link(): accept DOM-ID

--- lib/helpers/javascript_helper.php
+++ lib/helpers/javascript_helper.php
@@ -1,8 +1,13 @@
 <?# $Id$ ?>
 <?
 
-   function toggle_link($title) {
-      return link_to($title, '#', array('onclick' => '$(this.nextSibling).toggle(); return false'));
+   function toggle_link($title, $id=null) {
+      if ($id) {
+         $onclick = "\$('$id').toggle(); return false";
+      } else {
+         $onclick = '$(this.nextSibling).toggle(); return false';
+      }
+      return link_to($title, '#', array('onclick' => $onclick));
    }
 
 ?>
