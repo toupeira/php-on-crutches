1e057b0b3110fb2daa806cfed244472b4ab1a5e7
Markus Koller <markus-koller@gmx.ch>
2008-05-08 14:13:33 +0000

    toggle_link(): allow $options

--- lib/helpers/javascript_helper.php
+++ lib/helpers/javascript_helper.php
@@ -1,13 +1,13 @@
 <?# $Id$ ?>
 <?
 
-   function toggle_link($title, $id=null) {
+   function toggle_link($title, $id=null, $options=null) {
       if ($id) {
          $onclick = "\$('$id').toggle(); return false";
       } else {
          $onclick = '$(this.nextSibling).toggle(); return false';
       }
-      return link_to($title, '#', array('onclick' => $onclick));
+      return link_to($title, '#', array_merge((array) $options, array('onclick' => $onclick)));
    }
 
 ?>
