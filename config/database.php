a43aa3f864b3429deb7cbd46dd710ea12568970f
Markus Koller <markus-koller@gmx.ch>
2008-09-19 20:08:01 +0000

    config/database.php: tweak samples

--- config/database.php
+++ config/database.php
@@ -4,6 +4,7 @@
       #
       # SQLite example:
       #
+      #/*
       'sqlite' => array(
          'driver'   => 'sqlite',
 
@@ -12,21 +13,24 @@
          'development' => array('database' => DB.'development.db'),
          'test'        => array('database' => DB.'test.db'),
       ),
+      #*/
 
       #
       # MySQL example:
       #
-      #'mysql' => array(
-      #   'driver'   => 'mysql',
-      #   'hostname' => 'localhost',
-      #   'username' => '',
-      #   'password' => '',
-      #
-      #   # Specify different databases for environments
-      #   'production'  => array('database' => NAME.'_production'),
-      #   'development' => array('database' => NAME.'_development'),
-      #   'test'        => array('database' => NAME.'_test'),
-      #),
+      /*
+      'mysql' => array(
+         'driver'   => 'mysql',
+         'hostname' => 'localhost',
+         'username' => '',
+         'password' => '',
+      
+         # Specify different databases for environments
+         'production'  => array('database' => NAME.'_production'),
+         'development' => array('database' => NAME.'_development'),
+         'test'        => array('database' => NAME.'_test'),
+      ),
+      */
    );
 
 ?>
