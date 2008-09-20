e4ef3169f6df815738fcd801ddfe6924fcd3d361
Markus Koller <markus-koller@gmx.ch>
2008-09-20 21:57:04 +0000

    * script/translate: use separate templates for framework, application and database strings * tweak debug error page

--- lib/script/translate.php
+++ lib/script/translate.php
@@ -10,34 +10,80 @@
 
    require_once dirname(__FILE__).'/../script.php';
 
+   putenv('LANG=en_US.UTF-8');
+   putenv('LANGUAGE=');
+
+   $header = <<<TXT
+msgid ""
+msgstr ""
+"MIME-Version: 1.0\\n"
+"Content-Type: text/plain; charset=UTF-8\\n"
+"Content-Transfer-Encoding: 8bit\\n"
+
+
+TXT;
+
+   $templates = array('framework', 'application', 'attributes', 'additional');
    $languages = (array) config('languages');
    $domain = config('name');
 
-   $framework = LANG.'framework.pot';
-   $application = LANG.'application.pot';
-   $additional = LANG.'additional.pot';
+   function xgettext($template, $dir=null) {
+      $files = find_files($dir, '-name "*.php" -o -name "*.thtml"');
+      $files = implode(' ', array_map(escapeshellarg, $files));
 
-   $framework_files = find_files(LIB, '-name "*.php" -o -name "*.thtml"');
-   $framework_files = implode(' ', array_map(escapeshellarg, $framework_files));
+      print "Updating $template messages...\n";
+      $file = LANG.$template.'.pot';
+      file_put_contents($file, $GLOBALS['header']);
+      $command = "";
 
-   $application_files = find_files(APP, '-name "*.php" -o -name "*.thtml"');
-   $application_files = implode(' ', array_map(escapeshellarg, $application_files));
+      if (!run("xgettext -j --omit-header --from-code=utf-8 -L php -o '$file' $files")) {
+         print "Error while running xgettext\n\n";
+         exit(1);
+      }
+
+      if (`wc -l $file` > 6) {
+         system("sed -ri 's|^#: ".ROOT."|#: |' $file");
+      } else {
+         unlink($file);
+      }
+   }
 
    print "\n";
 
-   print "Updating framework messages...\n";
-   if (!run("xgettext --from-code=utf-8 -L php -o '$framework' $framework_files")) {
-      print "Error while running xgettext\n\n";
-      exit(1);
-   }
+   xgettext('framework', LIB);
+   xgettext('application', APP);
 
-   print "Updating application messages...\n";
-   if (!run("xgettext --from-code=utf-8 -L php -o '$application' $application_files")) {
-      print "Error while running xgettext\n\n";
-      exit(1);
-   }
+   print "Updating database attributes...\n";
 
-   print "Updating language templates...\n";
+   $attributes = array();
+   $file = fopen(LANG.'attributes.pot', 'w');
+   fwrite($file, $header);
+
+   foreach (glob(MODELS.'*.php') as $model) {
+      require_once $model;
+
+      $class = camelize(substr(basename($model), 0, -4));
+      if (is_subclass_of($class, Model)) {
+         $model = new $class();
+         $messages = array();
+         foreach ($model->attributes as $key => $value) {
+            $key = humanize($key);
+            if (!in_array($key, $attributes)) {
+               $attributes[] = $key;
+               $messages[] = "msgid \"$key\"\n"
+                           . "msgstr \"\"\n\n";
+            }
+         }
+
+         if ($messages) {
+            fwrite($file, "# $class attributes: \n");
+            fwrite($file, implode('', $messages));
+         }
+      }
+   }
+   fclose($file);
+
+   print "\nUpdating translations...\n";
 
    foreach ($languages as $language) {
       print "  $language: ";
@@ -46,27 +92,28 @@
       rm_f($all);
       touch($all);
       
-      foreach (array('framework', 'application', 'additional') as $master) {
-         $messages = "$path$master.po";
+      foreach ($templates as $template) {
+         $messages = "$path$template.po";
+         $template = LANG.$template.'.pot';
 
          if (file_exists($messages)) {
             # Merge existing translations
-            run("msgmerge -qU '$messages' '{$$master}'");
-         } elseif (is_file($$master)) {
+            run("msgmerge -qU '$messages' '$template'");
+         } elseif (is_file($template)) {
             # Create empty translations
             @mkdir(dirname($messages), 0750, true);
-            copy($$master, $messages);
+            copy($template, $messages);
          } else {
             continue;
          }
 
-         if (!run("msgcat --use-first --to-code=utf-8 -o '$all' '$all' '$messages'")) {
+         if (!run("msgcat --unique --to-code=utf-8 -o '$all' '$all' '$messages'")) {
             print "Error while running msgcat\n\n";
             exit(1);
          }
       }
 
-      # Compile message catalog
+      # Compile translated message catalog
       run("msgfmt -vo '$path$domain.mo' '$all'");
    }
 
