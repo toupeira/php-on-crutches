290ccdeb11e0c70361ee2abc623be46e7e7cebc7
Markus Koller <markus-koller@gmx.ch>
2008-10-09 20:09:17 +0000

    page_links(): only show the current range of pages

--- lib/helpers/query_set_helper.php
+++ lib/helpers/query_set_helper.php
@@ -12,18 +12,36 @@
    }
 
    function page_links($query_set) {
+      $current = $query_set->page;
+      $pages = $query_set->pages;
+
+      $start = max(1, $current - 5);
+      $end = min($pages, $current + 5);
+      if ($start == 1) $end = 11;
+      if ($end == $pages) $start = $pages - 10;
+
       $links = array();
-      for ($page = 1; $page <= $query_set->pages; $page++) {
+      for ($page = $start; $page <= $end; $page++) {
          $link = page_link($page);
 
-         if ($query_set->page == $page) {
+         if ($page == $current) {
             $link = "<strong>$link</strong>";
          }
 
          $links[] = $link;
       }
 
-      return implode(' ', $links);
+      $links = implode(' ', $links);
+
+      if ($start > 1) {
+         $links = "... $links";
+      }
+
+      if ($end < $pages) {
+         $links .= " ...";
+      }
+
+      return $links;
    }
 
 ?>
