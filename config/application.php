1e32983cd70da2812d02ea2582aaa2bdaabe9d3c
Markus Koller <markus-koller@gmx.ch>
2008-11-08 00:22:36 +0000

    update default files

--- config/application.php
+++ config/application.php
@@ -3,10 +3,15 @@
    $_CONFIG['application'] = array(
       'name'           => basename(ROOT),
       'languages'      => array('en'),
-      'trusted_hosts'  => array('127.0.0.1'),
+
       #'mail_sender'    => '',
       #'mail_from'      => '',
       #'mail_from_name' => '',
+
+      # Change this to improve security
+      'secret_key'     => 'c98df57338cb59822da92faa6f668ec5363b57e1',
+
+      'trusted_hosts'  => array('127.0.0.1'),
    );
 
 ?>
