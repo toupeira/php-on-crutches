17e32a7b75f1e77ac2ea05a2efe34ba66d912f52
Markus Koller <markus-koller@gmx.ch>
2008-11-08 17:50:34 +0000

    form_tag(): don't set ID on form token

--- lib/helpers/form_helper.php
+++ lib/helpers/form_helper.php
@@ -27,7 +27,7 @@
 
       # Add a token to POST forms
       if ($options['method'] == 'post' and $token = form_token()) {
-         $form .= hidden_field('_form_token', $token, array('force' => true)).N;
+         $form .= hidden_field('_form_token', $token, array('id' => null, 'force' => true)).N;
       }
 
       return $form;
