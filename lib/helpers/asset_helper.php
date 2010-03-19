df1c1d73cc7eb6082b762b521e07bfb12219cd1f
Markus Koller <markus-koller@gmx.ch>
2010-03-19 14:14:46 +0000

    asset_path(): generate URLs for model instances

--- lib/helpers/asset_helper.php
+++ lib/helpers/asset_helper.php
@@ -146,6 +146,10 @@
 
    # Build the path for an asset, append the last modification time for local files
    function asset_path($directory, $file, $ext='') {
+      if ($file instanceof Model) {
+         $file = url_for($file->to_params('show'));
+      }
+
       if ($file[0] == '/' or preg_match('#^\w+://.#', $file)) {
          # Leave absolute paths and fully-qualified URLs alone
          return $file;
