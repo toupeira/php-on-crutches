68aa60cfb9d4453c47c6b241023dcecbe6c96249
Markus Koller <markus-koller@gmx.ch>
2010-08-14 03:11:31 +0000

    init.php: initialize error reporting

--- lib/init.php
+++ lib/init.php
@@ -7,7 +7,8 @@
 # $Id$
 #
 
-   # Always display errors until the framework is initialized
+   # Initialize error reporting
+   error_reporting(E_ALL & ~E_NOTICE | E_STRICT);
    ini_set('display_errors', true);
 
    # Load core libraries
